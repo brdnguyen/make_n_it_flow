ecc3103da7c4aa84c226adcdc5165d0b50ceb7b1
Brad Nguyen <brad.nguyen@redbubble.com>
2015-12-11 14:55:30 +1100

    Initial static site template app

---
+++ public/js/startup.js
@@ -0,0 +1,83 @@
+
+$(document).ready(function() {
+    count = 0 ;
+    setTimeout(function() {
+        $('.pop-up,.pop-up-overlay').addClass('visible'); 
+    }, 500);
+
+    $('.each-img').click(function() {
+        $(this).toggleClass('active-img');
+        count = count + 1;
+        if (count == 2) {
+            // print 'personalising for XXXXX'
+            $('.message').show();
+            setTimeout(
+              function() {
+                $('.pop-up, .pop-up-overlay').hide();
+
+                $('#circle').circleProgress({
+                    value: 0.75,
+                    size: 80,
+                    fill: {
+                        gradient: ["red", "orange"]
+                    }
+                });
+                }, 3000
+            );
+            
+        }
+    });
+
+
+    $('.responsive').slick({
+        // dots: true,
+        infinite: true,
+        speed: 200,
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        swipeToSlide:1,
+        variableWidth: true,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 6,
+                slidesToScroll: 1,
+                // centerMode: true,
+
+            }
+
+        }, {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 2,
+                dots: true,
+                infinite: true,
+
+            }
+
+
+        }, {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                dots: true,
+                infinite: true,
+                
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true,
+                infinite: true,
+                autoplay: true,
+                autoplaySpeed: 2000,
+            }
+        }]
+    });
+
+
+});
