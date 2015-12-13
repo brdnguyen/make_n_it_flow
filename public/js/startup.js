ec54261e515247e18ecb94b71e0cfca67c4a3149
Brad Nguyen <brad.nguyen@redbubble.com>
2015-12-13 20:07:34 +1100

    disable responsive in startup.js

--- public/js/startup.js
+++ public/js/startup.js
@@ -37,47 +37,48 @@ $(document).ready(function() {
         slidesToShow: 8,
         slidesToScroll: 1,
         swipeToSlide:1,
-        variableWidth: true,
-        responsive: [{
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 6,
-                slidesToScroll: 1,
-                // centerMode: true,
+        variableWidth: true
+        //,
+        // responsive: [{
+        //     breakpoint: 1024,
+        //     settings: {
+        //         slidesToShow: 6,
+        //         slidesToScroll: 1,
+        //         // centerMode: true,
 
-            }
+        //     }
 
-        }, {
-            breakpoint: 800,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 2,
-                dots: true,
-                infinite: true,
+        // }, {
+        //     breakpoint: 800,
+        //     settings: {
+        //         slidesToShow: 3,
+        //         slidesToScroll: 2,
+        //         dots: true,
+        //         infinite: true,
 
-            }
+        //     }
 
 
-        }, {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-                dots: true,
-                infinite: true,
+        // }, {
+        //     breakpoint: 600,
+        //     settings: {
+        //         slidesToShow: 2,
+        //         slidesToScroll: 2,
+        //         dots: true,
+        //         infinite: true,
                 
-            }
-        }, {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                dots: true,
-                infinite: true,
-                autoplay: true,
-                autoplaySpeed: 2000,
-            }
-        }]
+        //     }
+        // }, {
+        //     breakpoint: 480,
+        //     settings: {
+        //         slidesToShow: 1,
+        //         slidesToScroll: 1,
+        //         dots: true,
+        //         infinite: true,
+        //         autoplay: true,
+        //         autoplaySpeed: 2000,
+        //     }
+        // }]
     });
 
 
